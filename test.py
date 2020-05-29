0b4c24c4041a4040a069419981d16e19f9fe4c14
Rahmanzia3 <38445942+Rahmanzia3@users.noreply.github.com>
2020-05-29 20:30:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,13 +3,17 @@ from argparse import ArgumentParser
 import argparse
 
 if __name__ == "__main__":
-	parser = ArgumentParser()
-	parser = argparse.ArgumentParser(description='Process some integers.')
-	parser.add_argument('--numbers', metavar='N',required=True, type=int, 
-		nargs='+',help='enter numbers you want to add')
+	# parser = ArgumentParser()
+	# parser = argparse.ArgumentParser(description='Process some integers.')
+	# parser.add_argument('--numbers', metavar='N',required=True, type=int, 
+	# 	nargs='+',help='enter numbers you want to add')
 
-	opt = parser.parse_args()
+	# opt = parser.parse_args()
 
-	numbers = opt.numbers
+	# numbers = opt.numbers
 
+	# numbers = input("Enter a two value: ")
+
+
+	numbers = list(map(int, input("Enter a multiple value: ").split())) 
 	print(sum(numbers))
